7da20fec507c2bccfb70186371769ad0991698dd
aakashDeveloper <ahanda205@gmail.com>
2019-03-15 21:53:36 +0530

    a

--- firstapp/src/app/app.component.ts
+++ firstapp/src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-main',
-    template: `<h1>This is my firstcomponent</h1>
-                <app-book></app-book>`
+    templateUrl: './app.component.html'
 })
 
 export class AppComponent {
-
+    title: String = '***Home Page***';
+    marks: Number = 10;
 }
+
+
+
+/*
+template: `<h1>This is my firstcomponent</h1>
+                <app-book></app-book>`*/
