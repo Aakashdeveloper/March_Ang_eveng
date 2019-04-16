ee243f05cabe385e620fd36235cdf1666504b6cc
aakashDeveloper <ahanda205@gmail.com>
2019-04-16 22:06:50 +0530

    a

--- templateApproch/src/app/employeeForm/form.component.ts
+++ templateApproch/src/app/employeeForm/form.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { Employee } from '../models/employee.model';
 
 @Component({
     selector: 'app-form',
@@ -7,4 +8,13 @@ import {Component} from '@angular/core';
 
 export class EmployeeFormComponent {
     languages: any[] = ['Node', 'Angular', 'React', 'Mongo'];
+    model = new Employee('John', 'Bill', 'a@a.com', '1234', true, 'male' , 'Angular');
+
+    firstToUpper(name: string): void {
+        if (name.length > 0) {
+            this.model.firstname = name.charAt(0).toUpperCase() + name.slice(1);
+        } else {
+            this.model.firstname = name;
+        }
+    }
 }
